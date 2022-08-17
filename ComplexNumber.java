eefd48eef8518cc599f043676cc51e88f0fa9427
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2022-08-17 22:01:34 +0300

    Add files via upload

---
+++ ComplexNumber.java
@@ -0,0 +1,42 @@
+package com.challenge;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public void add(ComplexNumber number) {
+        this.real += number.getReal();
+        this.imaginary += number.getImaginary();
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void subtract(ComplexNumber number) {
+        this.real -= number.getReal();
+        this.imaginary -= number.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+
+}
